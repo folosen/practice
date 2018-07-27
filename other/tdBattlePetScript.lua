faf4c0ee2dec66340b6c6814c815aea693fc3828
folosen <frozenyihan@sina.cn>
2018-07-27 21:07:25 +0800

    Update tdBattlePetScript.lua

--- other/tdBattlePetScript.lua
+++ other/tdBattlePetScript.lua
@@ -1,19 +1,25 @@
 if [ round = 1 ]
     use(#1)
 endif
+
 if [ round = 2 ]
     change(#2)
 endif
+
 standby(待命) [ self.aura(昏迷).exists ]
+
 if [ round > 2 & self.dead ]
     change(#3)
 endif
+
 if [ round > 2 & self.dead ]
     quit(认输) [ self(#3).dead ]
 endif
-if [  enemy.hp > 600 ]
+
+if [  enemy.hp > 600 & !enemy.speed.fast ]
     ability(#1)
 endif
+
 ability(#2) [ self.ability(#2).usable ]
 ability(#3) [ self.ability(#3).usable ]
 ability(#1)
