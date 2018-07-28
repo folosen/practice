0d4d97ab70c52b29b6d14726b2af3ddd9d060c40
folosen <frozenyihan@sina.cn>
2018-07-28 15:14:35 +0800

    Update tdBattlePetScript.lua

--- other/tdBattlePetScript.lua
+++ other/tdBattlePetScript.lua
@@ -1,25 +1,27 @@
+standby(待命) [ self.aura(昏迷).exists ]
+catch(抓宠) [ enemy.quality = 4 ]
+
 if [ round = 1 ]
     use(#1)
 endif
 
 if [ round = 2 ]
-    change(#2)
-endif
-
-standby(待命) [ self.aura(昏迷).exists ]
-
-if [ round > 2 & self.dead ]
-    change(#3)
+    change(next)
 endif
 
 if [ round > 2 & self.dead ]
-    quit(认输) [ self(#3).dead ]
+    quit(认输) [ self(#1).active ]
+    change(next)
 endif
 
-if [  enemy.hp > 600 & !enemy.speed.fast ]
+if [  enemy.hp > 600 & self.speed.fast ]
     ability(#1)
 endif
 
+ability(#3) [ enemy.type = 飞行 &  enemy.hp > 400 ]
+
 ability(#2) [ self.ability(#2).usable ]
-ability(#3) [ self.ability(#3).usable & enemy.speed.fast ]
+
+ability(#3) [ enemy.type = 飞行 ]
+
 ability(#1)
